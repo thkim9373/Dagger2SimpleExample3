9e542a4390b8dd35af4e11eb12a113e8213c3832
TaeHoon <thkim9373@gmail.com>
2020-07-18 23:50:15 +0900

    save commit.

---
+++ app/src/main/java/com/hoony/dagger2simpleexample3/Info.kt
@@ -0,0 +1,7 @@
+package com.hoony.dagger2simpleexample3
+
+import javax.inject.Inject
+
+class Info @Inject constructor() {
+    val text = "Hello Dagger 2"
+}
--- app/src/main/java/com/hoony/dagger2simpleexample3/MainActivity.kt
+++ app/src/main/java/com/hoony/dagger2simpleexample3/MainActivity.kt
@@ -2,10 +2,18 @@ package com.hoony.dagger2simpleexample3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var info: Info
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        DaggerMagicBox.create().poke(this)
+        tvText.text = info.text
     }
 }
