88299e074a6e345c9f32141224a4818fb3706843
TaeHoon <thkim9373@gmail.com>
2020-08-03 23:34:01 +0900

    save commit.

---
+++ app/src/main/java/com/hoony/dagger2simpleexample3/chaper4/MagicScope.kt
@@ -0,0 +1,7 @@
+package com.hoony.dagger2simpleexample3.chaper4
+
+import javax.inject.Scope
+
+@Scope
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+annotation class MagicScope
--- app/src/main/java/com/hoony/dagger2simpleexample3/chapter5/Chapter5Activity.kt
+++
@@ -1,6 +0,0 @@
-package com.hoony.dagger2simpleexample3.chapter5
-
-import androidx.appcompat.app.AppCompatActivity
-
-class Chapter5Activity : AppCompatActivity() {
-}
