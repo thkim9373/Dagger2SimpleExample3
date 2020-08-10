ce8d6f9ac58170821102fd5c6458f5e0c9354e60
TaeHoon <thkim9373@gmail.com>
2020-08-10 22:21:40 +0900

    save commit.

--- app/src/main/java/com/hoony/dagger2simpleexample3/chaper5/MagicScope.kt
+++ app/src/main/java/com/hoony/dagger2simpleexample3/chaper5/MagicScope.kt
@@ -2,6 +2,7 @@ package com.hoony.dagger2simpleexample3.chaper5
 
 import javax.inject.Inject
 import javax.inject.Scope
+import javax.inject.Singleton
 
 @Scope
 @kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
@@ -9,11 +10,16 @@ annotation class MagicScope
 
 var staticCounter: Int = 0
 
+@Singleton
+class SingletonOne @Inject constructor() {
+    val count = staticCounter++
+}
+
 @MagicScope
 class UniqueMagic @Inject constructor() {
-    val counter = staticCounter++
+    val count = staticCounter++
 }
 
 class NormalMagic @Inject constructor() {
-    val counter = staticCounter++
+    val count = staticCounter++
 }
